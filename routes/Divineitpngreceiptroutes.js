289374bc06680fb942a384832932c8d19b2c26b8
Ananddivine <ananddivine85@gmail.com>
2026-08-28 13:27:07 +1000

    Initial clean commit

--- routes/Divineitpngreceiptroutes.js
+++ routes/Divineitpngreceiptroutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const receiptController = require("../controllers/divineitpngReceiptController");
+const receiptController = require("../controllers/DivineitpngReceiptController");
 
 // NOTE: keep specific routes (last, job/last, search-job) ABOVE "/:id"
 // so Express doesn't treat "last" or "job" as an :id param.
